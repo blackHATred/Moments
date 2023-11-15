0a01ee394b9bda26e2a0835f26a6489257b3c47d
smail <smailcmail@gmail.com>
2023-11-15 06:45:41 +0300

    update

--- handlers/CacheHandler.py
+++ handlers/CacheHandler.py
@@ -1,6 +1,9 @@
 from pymemcache.client import base
 
-from config import memcached_server
+try:
+    from config import memcached_server
+except ModuleNotFoundError:
+    from config_example import memcached_server
 
 # memcache клиент
 mc = base.Client(server=memcached_server)
